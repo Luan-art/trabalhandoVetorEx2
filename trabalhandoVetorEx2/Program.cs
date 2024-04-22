c4919ad3f3716698cc0022fe6a36a20fb887e7d7
Luan-art <luanlf2003@gmail.com>
2024-04-22 19:15:36 -0300

    Add project files.

---
+++ trabalhandoVetorEx2/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] numeros = new int[10];
+int[] impares =  new int[10];
+int MaxValue = 10, contadorImp = 0;
+
+Console.WriteLine("Digite um numero: ");
+
+for (int i = 0; i < MaxValue; i++)
+{
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nImpressão:");
+
+for (int i = 0; i < MaxValue; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+Console.WriteLine("\nInvertido:");
+
+for (int i = 9; i >= 0; i--)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+Console.WriteLine("\nImpressão pela metade:");
+
+for (int i = 0; i < MaxValue / 2; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+
+for (int i = 0; i < MaxValue; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        impares[contadorImp] = numeros[i];
+        contadorImp++;
+    }
+}
+
+Console.WriteLine("\nImpares:");
+
+for (int i = 0; i < contadorImp; i++)
+{
+    Console.WriteLine(impares[i]);
+}
